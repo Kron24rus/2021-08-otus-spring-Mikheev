f2a59c2da9e5e393fb69ca60e49033cf9e8662e2
Alexander Mikheev <alexanderkronmikheev@gmail.com>
2021-09-05 02:30:08 +0300

    improved test

--- homeworkone/src/test/java/com/mikheev/homework/services/impl/SurveyServiceImplTest.java
+++ homeworkone/src/test/java/com/mikheev/homework/services/impl/SurveyServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.mikheev.homework.services.impl;
 
+import com.mikheev.homework.configuration.SurveyConfiguration;
 import com.mikheev.homework.services.IOService;
 import com.mikheev.homework.services.SurveyService;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class SurveyServiceImplTest {
@@ -17,17 +17,25 @@ public class SurveyServiceImplTest {
     @MockBean
     private IOService ioService;
 
+    @MockBean
+    private SurveyConfiguration surveyConfiguration;
+
     @Autowired
     private SurveyService surveyServiceCorrect;
 
-//    @BeforeEach
-//    void setUp() {
-//        Mockito.reset(ioService);
-//    }
-//
     @Test
     void checkCorrectCsvResultParsing() {
+        when(surveyConfiguration.getDefaultQuestionsPath()).thenReturn("/test-csv/questions-correct.csv");
         surveyServiceCorrect.run();
         verify(ioService, atLeastOnce()).write(anyString());
+        verify(ioService, atLeastOnce()).readFromConsole();
+    }
+
+    @Test
+    void checkWrongCsvResultParsing() {
+        when(surveyConfiguration.getDefaultQuestionsPath()).thenReturn("/test-csv/questions-broken.csv");
+        surveyServiceCorrect.run();
+        verify(ioService, times(1)).write("Error occurred during running application.");
+        verify(ioService, never()).readFromConsole();
     }
 }
